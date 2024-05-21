16a8482395d91d0f70a162a0027f0066bd8b0917
Cedric Durau <cedric.durau@gmail.com>
2024-05-21 21:43:40 +0200

    Add Footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css"
 import "./styles.css"
 
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
     return (
@@ -10,9 +11,7 @@ function App() {
             <div className="container">
                 <Header />
             </div>
-            <footer className="footer">
-                <p>Footer content here</p>
-            </footer>
+            <Footer />
         </div>
     )
 }
