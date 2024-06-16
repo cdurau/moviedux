0920b0092350a9a9259404d88e0cd0c46fd2ae7e
Cedric Durau <cedric.durau@gmail.com>
2024-06-16 11:01:56 +0200

    Add ReactRouter + WatchList

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Header from "./components/Header"
 import MoviesGrid from "./components/MoviesGrid"
 import Footer from "./components/Footer"
+import Watchlist from "./components/Watchlist"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+
 import "./App.css"
 import "./styles.css"
 
@@ -9,7 +12,22 @@ function App() {
         <div className="App">
             <div className="container">
                 <Header />
-                <MoviesGrid />
+                <Router>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/watchlist">Watchlist</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <Routes>
+                        <Route path="/" element={<MoviesGrid />} />
+                        <Route path="/watchlist" element={<Watchlist />} />
+                    </Routes>
+                </Router>
             </div>
             <Footer />
         </div>
